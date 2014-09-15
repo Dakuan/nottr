a07e9f7745c46608830be67d24b6617e32919790
Dakuan <dom.barker808@gmail.com>
2014-09-15 22:53:33 +0100

    comparator to show most recent at top

--- client/js/app/models/updates-collection.js
+++ client/js/app/models/updates-collection.js
@@ -6,5 +6,9 @@ Backbone.$ = require('jquery');
 module.exports = Backbone.Collection.extend({
     url: '/api/updates',
     model: require('./updates-model'),
-    comparator: 'sentiment'
+    comparator: function (model) {
+    	var m = moment(model.get('updatedAt'));
+    	return -m.toDate();
+    }
+    // comparator: 'sentiment'
 });
