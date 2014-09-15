a6b3483c9be7438a5e54ef43e1ec6418013a062b
Dakuan <dom.barker808@gmail.com>
2014-09-15 19:03:46 +0100

    basic dynamic data

--- client/js/app/app.js
+++ client/js/app/app.js
@@ -5,11 +5,20 @@
 console.log('hello from app');
 
 var React = require('react'),
+	_ = require('underscore'),
     MainScreen = require('./components/screens/MainScreen'),
     app = document.getElementById('app');
 
 // hack to get the React chrome extension to work
 window.React = React;
 
+var updates = _(_.range(10)).map(function (index) {
+    return {
+    	id: index,
+        userHandle: '@lemonzestt',
+        message: 'Lemonade works for mee'
+    }
+});
+
 // go!
-React.renderComponent(<MainScreen />, app);  
+React.renderComponent(<MainScreen updates={updates} />, app);
