6fc22766a5e5cb3b8c97a68e7b5604cb281953ed
Dakuan <dom.barker808@gmail.com>
2014-09-15 20:32:48 +0100

    include pipe

--- server/routes/api/updates-api-routes.js
+++ server/routes/api/updates-api-routes.js
@@ -2,11 +2,13 @@ var express = require('express'),
     request = require('request'),
     transformStream = require('../../transforms/transform-stream'),
     camelCaseKeys = require('../../transforms/camel-case-keys'),
+    assignSentiment = require('../../transforms/assign-sentiment'),
     updatesRouter = express.Router();
 
 updatesRouter.get('/', function (req, res, next) {
     request.get('http://adaptive-test-api.herokuapp.com/tweets.json')
         .pipe(transformStream(camelCaseKeys))
+        .pipe(transformStream(assignSentiment))
         .pipe(res);
 });
 
