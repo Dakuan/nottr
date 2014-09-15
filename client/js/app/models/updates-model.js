1f46472ae87ac04d179aac3b4dcf4a661aa007e3
Dakuan <dom.barker808@gmail.com>
2014-09-15 20:19:17 +0100

    show sentiment dynamically

--- client/js/app/models/updates-model.js
+++ client/js/app/models/updates-model.js
@@ -1,7 +1,23 @@
-var Backbone = require('backbone');
+var Backbone = require('backbone'),
+    sentiment = require('../../../../constants/sentiment');
+
+function _sentimentIcon(model) {
+    switch (model.sentiment) {
+    case sentiment.POSITIVE:
+        model.sentimentIcon = 'fa-smile-o'
+        break;
+    case sentiment.NEGATIVE:
+        model.sentimentIcon = 'fa-frown-o'
+        break;
+    default:
+        model.sentimentIcon = 'fa-meh-o'
+        break;
+    }
+    return model;
+}
 
 module.exports = Backbone.Model.extend({
     parse: function (model) {
-        return model;
+    	return _sentimentIcon(model);
     }
 });
