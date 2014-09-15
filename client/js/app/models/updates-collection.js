60378204af5595e5bde1c71edc12c69d79259533
Dakuan <dom.barker808@gmail.com>
2014-09-15 22:40:04 +0100

    sort by sentiment

--- client/js/app/models/updates-collection.js
+++ client/js/app/models/updates-collection.js
@@ -1,8 +1,10 @@
-var Backbone = require('backbone');
+var Backbone = require('backbone'),
+    moment = require('moment');
 
 Backbone.$ = require('jquery');
 
 module.exports = Backbone.Collection.extend({
     url: '/api/updates',
-    model: require('./updates-model')
+    model: require('./updates-model'),
+    comparator: 'sentiment'
 });
