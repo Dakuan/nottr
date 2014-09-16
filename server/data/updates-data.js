99d1e747a9bb45907184a7a9ebd7750b06c7d62a
Dakuan <dom.barker808@gmail.com>
2014-09-17 00:48:05 +0100

    clean up data access

--- server/data/updates-data.js
+++ server/data/updates-data.js
@@ -1,12 +1,27 @@
 var request = require('request'),
     transformStream = require('../transforms/transform-stream'),
     camelCaseKeys = require('../transforms/camel-case-keys'),
-    assignSentiment = require('../transforms/assign-sentiment');
+    assignSentiment = require('../transforms/assign-sentiment'),
+    Q = require('q');
 
 module.exports = {
     get: function () {
         return request.get('http://adaptive-test-api.herokuapp.com/tweets.json')
             .pipe(transformStream(camelCaseKeys))
             .pipe(transformStream(assignSentiment));
+    },
+    getPromise: function () {
+        var deferred = Q.defer();
+            updates = this.get(),
+            u = '';
+        updates.on('data', function (data) {
+            u += data;
+        });
+        updates.on('finish', function () {
+            deferred.resolve(JSON.parse(u));
+            u = '';
+        });
+
+        return deferred.promise;
     }
 }
